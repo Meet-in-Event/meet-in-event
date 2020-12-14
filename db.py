955ddc825e052bd6e0e14cadc2a74d710b39b032
MelindaFang-code <67951224+MelindaFang-code@users.noreply.github.com>
2020-12-14 18:50:31 +0800

    Update db.py

--- db.py
+++ db.py
@@ -1,21 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-import base64
-import boto3
-import datetime
-from io import BytesIO
-from mimetypes import guess_extension, guess_type #know what type of file we are parsing
-import os #know the directory we are working at
-from PIL import Image
-import random
-import re
-import string
+
 
 db = SQLAlchemy()
-
-EXTENSIONS = ["png", "gif", "jpg", "jpeg"]
-BASE_DIR = os.getcwd()
-S3_BUCKET = "melindademo"
-S3_BASE_URL = f"https://{S3_BUCKET}.s3-us-east-2.amazonaws.com"
 
 
 association_table_1 = db.Table(
@@ -202,78 +188,4 @@ class Friend(db.Model):
             "friend_netid": self.friend_netid
         }
 
-class Asset(db.Model):
-    __tablename__ = "asset"
 
-    id = db.Column(db.Integer, primary_key = True)
-    base_url = db.Column(db.String, nullable=True)
-    salt = db.Column(db.String, nullable=False)
-    extension = db.Column(db.String, nullable=False)
-    width = db.Column(db.Integer, nullable=False)
-    height = db.Column(db.Integer, nullable=False)
-    created_at= db.Column(db.DateTime, nullable=False)
-
-    def __init__(self, **kwargs):
-        self.create(kwargs.get("image_data"))
-    
-    def serialize(self):
-        return{
-            "url":f"{self.base_url}/{self.salt}.{self.extension}",
-            "created_at": str(self.created_at),
-        } 
-    
-    def create(self, image_data):
-        try:
-            #base64 string(image_data) plug in guess_type returns
-            #--> .png --> [1:]strip of the first char: the period
-            ext = guess_extension(guess_type(image_data)[0])[1:]
-            if ext not in EXTENSIONS:
-                raise Exception(f"Extension {e} not supported!")
-            
-            #secure way of generating random string for image name 
-            salt = "".join(
-                random.SystemRandom().choice(
-                    string.ascii_uppercase + string.digits
-                )
-                for _ in range(16)         
-            )
-            
-            #remove header of base64 string and open image
-            img_str = re.sub("^data:image/.+;base64,", "", image_data)
-            #replace this part ^data:image/.+;base64 in image_data with ""
-            img_data = base64.b64decode(img_str)
-            img = Image.open(BytesIO(img_data))
-
-            self.base_url = S3_BASE_URL
-            self.salt = salt
-            self.extension = ext
-            self.width = img.width
-            self.height = img.height
-            self.created_at = datetime.datetime.now()
-            img_filename = f"{salt}.{ext}" 
-            self.upload(img, img_filename)
-        except Exception as e:
-            print(f"unable to create image due to {e}")
-        
-    
-    def upload(self, img, img_filename):
-        try:
-            #save iamge temporarily on server
-            img_temploc = f"{BASE_DIR}/{img_filename}"
-            img.save(img_temploc) #save image at this location
-            print("Here")
-            #upload iamge to S3
-            s3_client = boto3.client("s3")
-            s3_client.upload_file(img_temploc, S3_BUCKET, img_filename)
-            print("Here2")
-            
-            #make S3 image url public
-            s3_resource = boto3.resource("s3")
-            object_acl = s3_resource.ObjectAcl(S3_BUCKET, img_filename)
-            object_acl.put(ACL="public-read")
-            print("Here3")
-
-            os.remove(img_temploc)
-
-        except Exception as e:
-            print(f"unable to create image due to {e}")
